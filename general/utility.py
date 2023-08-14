f6f62dc5f9cda032f448ebdc4209a0a0eda0dde9
Felix Xu <61252303+FelixXu35@users.noreply.github.com>
2023-08-14 13:20:05 +0100

    add quantum states generator

--- general/utility.py
+++ general/utility.py
@@ -12,5 +12,17 @@ def symmetric_matrix_generator(dim: int) -> np.array:
     mat = np.random.rand(dim**2).reshape(dim, dim)
     mat = np.triu(mat)
     mat += mat.T - np.diag(mat.diagonal())
-    
+
     return mat
+
+
+def all_quantum_states(n_qubits) -> list:
+    states = []
+    for i in range(2**n_qubits):
+        a = f"{bin(i)[2:]:0>{n_qubits}}"
+        n_ones = 0
+        for j in a:
+            if j == "1":
+                n_ones += 1
+        states.append(a)
+    return states
